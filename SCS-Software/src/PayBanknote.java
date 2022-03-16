a3324a6763a76ac7eb991da6764c5a5ab881d8e1
Chenghou <99150517+ChenghouSi@users.noreply.github.com>
2022-03-15 19:38:25 -0600

    SCS pay with banknote

--- SCS-Software/src/PayBanknote.java
+++ SCS-Software/src/PayBanknote.java
@@ -5,55 +5,13 @@ import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.BanknoteSlotObserver;
 
 public class PayBanknote implements BanknoteSlotObserver {
-	private Currency currency;
-	private int total, fiveBanknoteNum, tenBanknoteNum, twentyBanknoteNum, fiftyBanknoteNum, hundredBanknoteNum;
-	private boolean status;
-	
-	public PayBanknote(boolean status) {
-		this.status = status;
-		currency = Currency.getInstance("CAD");
-		fiveBanknoteNum = 0;
-		tenBanknoteNum = 0;
-		twentyBanknoteNum = 0;
-		fiftyBanknoteNum = 0;
-		hundredBanknoteNum = 0;
-		total = 0;
-	}
-	
-	public void addBankNote(Banknote inserted) {
-		int value = inserted.getValue();
-		total += value;
-		
-		if (value == 5) {
-			fiveBanknoteNum++;
-		}
-		else if (value == 10) {
-			tenBanknoteNum++;
-		}
-		else if (value == 20) {
-			twentyBanknoteNum++;
-		}
-		else if (value == 50) {
-			fiftyBanknoteNum++;
-		}
-		else if (value == 100) {
-			hundredBanknoteNum++;
-		}
-		else {
-			throw new SimulationException("Not a Valid Canadian Banknote!!!");
-		}
-	}
-	
-	public int getBanknoteTotal() {
-		return total;
-	}
 
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		status = true;	
+		// TODO Auto-generated method stub
 	}
 
 	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		status = false;	
+		// TODO Auto-generated method stub
 	}
 
 	public void banknoteInserted(BanknoteSlot slot) {
