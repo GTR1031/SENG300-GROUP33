484e69c74a29d97635877629090d39ba5ba85fb2
Joshua Cordeiro-Zebkowitz <zebkowitz@gmail.com>
2022-03-17 17:21:37 -0600

    Convert to only 2 classes

--- SCS-Software/src/CheckoutSystem.java
+++ SCS-Software/src/CheckoutSystem.java
@@ -1,31 +1,47 @@
+import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
+import org.lsmr.selfcheckout.devices.observers.TouchScreenObserver;
+import org.lsmr.selfcheckout.devices.observers.CoinSlotObserver;
 import java.util.Currency;
 import org.lsmr.selfcheckout.*;
 import org.lsmr.selfcheckout.devices.*;
-import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.BanknoteSlotObserver;
 
-public class PayBanknote implements BanknoteSlotObserver {
+public class CheckoutSystem implements CoinSlotObserver, BanknoteSlotObserver, TouchScreenObserver {
 
+	@Override
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
 		// TODO Auto-generated method stub
+		
 	}
 
+	@Override
 	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
 		// TODO Auto-generated method stub
+		
 	}
 
+	@Override
 	public void banknoteInserted(BanknoteSlot slot) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public void banknoteEjected(BanknoteSlot slot) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public void banknoteRemoved(BanknoteSlot slot) {
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void coinInserted(CoinSlot slot) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- SCS-Software/src/ScanAndWeigh.java
+++ SCS-Software/src/ScanAndWeigh.java
@@ -3,8 +3,10 @@ import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.BarcodeScanner;
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.BarcodeScannerObserver;
+import org.lsmr.selfcheckout.devices.ElectronicScale;
+import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
 
-public class ScanItem implements BarcodeScannerObserver {
+public class ScanAndWeigh implements BarcodeScannerObserver, ElectronicScaleObserver {
 
 	@Override
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
@@ -14,6 +16,24 @@ public class ScanItem implements BarcodeScannerObserver {
 
 	@Override
 	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void weightChanged(ElectronicScale scale, double weightInGrams) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void overload(ElectronicScale scale) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void outOfOverload(ElectronicScale scale) {
 		// TODO Auto-generated method stub
 		
 	}
