591b4199900c997a8b4990f403f167ec227b9473
Chenghou <99150517+ChenghouSi@users.noreply.github.com>
2022-03-17 19:24:12 -0600

    banknote and coin methods added

--- SCS-Software/src/CheckoutSystem.java
+++ SCS-Software/src/CheckoutSystem.java
@@ -1,47 +1,65 @@
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.TouchScreenObserver;
 import org.lsmr.selfcheckout.devices.observers.CoinSlotObserver;
+import org.lsmr.selfcheckout.devices.observers.CoinValidatorObserver;
+
+import java.math.BigDecimal;
 import java.util.Currency;
 import org.lsmr.selfcheckout.*;
 import org.lsmr.selfcheckout.devices.*;
 import org.lsmr.selfcheckout.devices.observers.BanknoteSlotObserver;
+import org.lsmr.selfcheckout.devices.observers.BanknoteValidatorObserver;
 
-public class CheckoutSystem implements CoinSlotObserver, BanknoteSlotObserver, TouchScreenObserver {
+public class CheckoutSystem implements CoinValidatorObserver, BanknoteValidatorObserver, TouchScreenObserver {
+	private Currency currency;
+	private double totalAmount;
+	private BigDecimal coinTotal;
+	private int banknoteTotal;
+	
+	
+	public CheckoutSystem(int total) {
+		currency = Currency.getInstance("CAD");
+		coinTotal = new BigDecimal("0");
+		banknoteTotal = 0;
+		totalAmount = total;
+	}
+	
+	public void finish() {
+		double totalInserted = coinTotal.doubleValue() + (double) banknoteTotal;
+	
+		if (totalInserted < totalAmount) {
+			throw new SimulationException("Not enough");
+		}
+	}
 
 	@Override
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void validCoinDetected(CoinValidator validator, BigDecimal value) {
+		coinTotal.add(value);
+	}
+
+	@Override
+	public void invalidCoinDetected(CoinValidator validator) {
 		
 	}
 
 	@Override
-	public void banknoteInserted(BanknoteSlot slot) {
-		// TODO Auto-generated method stub
-		
+	public void validBanknoteDetected(BanknoteValidator validator, Currency currency, int value) {
+		banknoteTotal += value;
 	}
 
 	@Override
-	public void banknoteEjected(BanknoteSlot slot) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void banknoteRemoved(BanknoteSlot slot) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void coinInserted(CoinSlot slot) {
-		// TODO Auto-generated method stub
-		
+	public void invalidBanknoteDetected(BanknoteValidator validator) {
+			
 	}
 
 }
