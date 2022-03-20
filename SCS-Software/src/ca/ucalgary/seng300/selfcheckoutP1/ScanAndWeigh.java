60e696c6ba797d2f4288eea036aa4fa0c68a6836
rei-tsunemi <reren1031@gmail.com>
2022-03-20 13:02:07 -0600

    Update ScanAndWeigh.java
    Commenting and added one new test function

--- SCS-Software/src/ca/ucalgary/seng300/selfcheckoutP1/ScanAndWeigh.java
+++ SCS-Software/src/ca/ucalgary/seng300/selfcheckoutP1/ScanAndWeigh.java
@@ -1,57 +1,172 @@
-package ca.ucalgary.seng300.selfcheckoutP1;
+/*
+ * SENG300 Group33
+ * Project iteration 1
+ * 
+ * Group member:
+ * Joshua Cordeiro-Zebkowitz
+ * Ethan Macson
+ * Spencer Mutch
+ * Chenghou Si
+ * Rei Tsunemi
+ * Nhat Truong
+ */
 
-import java.util.List;
+package Test;
 
-import org.lsmr.selfcheckout.Barcode;
-import org.lsmr.selfcheckout.BarcodedItem;
-import org.lsmr.selfcheckout.devices.AbstractDevice;
-import org.lsmr.selfcheckout.devices.BarcodeScanner;
-import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
-import org.lsmr.selfcheckout.devices.observers.BarcodeScannerObserver;
-import org.lsmr.selfcheckout.devices.ElectronicScale;
-import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import org.lsmr.selfcheckout.*;
+import org.lsmr.selfcheckout.devices.*;
 import org.lsmr.selfcheckout.products.BarcodedProduct;
 
-public class ScanAndWeigh implements BarcodeScannerObserver, ElectronicScaleObserver {
+import ca.ucalgary.seng300.selfcheckoutP1.*;
+
+public class TestCheckoutSystem {
+	// items
+	private double weight1, weight2, weight3;
+	private BarcodedItem item1, item2, item3;
+	private BarcodedProduct product1, product2, product3;
+	private List<BarcodedProduct> allProduct = new ArrayList<BarcodedProduct>();
+	private List<BarcodedItem> allItem = new ArrayList<BarcodedItem>();
+
+	// banknotes
+	private Currency currency;
+	private int[] banknoteDenomin;
+	private Banknote banknote_one, banknote_five, banknote_ten, banknote_twenty, banknote_fifty;
+
+	// coins
+	private BigDecimal[] coinDenomin;
+	private Coin penny, nickel, dime, quarter, loonie, tonnie;
 	
-	public ScanAndWeigh(List<BarcodedProduct> allProduct, List<BarcodedItem> allItem) {
-		// TODO Auto-generated constructor stub
+	// self-checkout station
+	private SelfCheckoutStation scs;
+	private CheckoutSystem checkout;
+
+	@Before
+	public void setupItemAndProduct() {
+		// setup barcode for each item/product
+		Numeral[] code1 = new Numeral[4];
+		Numeral[] code2 = new Numeral[4];
+		Numeral[] code3 = new Numeral[4];
+		
+		for (int i = 0; i < 3; i++) {
+			code1[i] = Numeral.zero;
+			code2[i] = Numeral.zero;
+			code3[i] = Numeral.zero;
+		}
+		
+		code1[3] = Numeral.one;
+		code2[3] = Numeral.two;
+		code3[3] = Numeral.three;
+		
+		Barcode barcode1 = new Barcode(code1);		// barcode for item1/product1 = '0001'
+		Barcode barcode2 = new Barcode(code2);		// barcode for item2/product2 = '0002'
+		Barcode barcode3 = new Barcode(code3);		// barcode for item3/product3 = '0003'	
+
+		// setup all products
+		product1 = new BarcodedProduct(barcode1, "Product 1", new BigDecimal("10"));
+		product2 = new BarcodedProduct(barcode2, "Product 2", new BigDecimal("0.05"));
+		product3 = new BarcodedProduct(barcode3, "Product 3", new BigDecimal("0.39"));
+
+		// setup all items
+		weight1 = 0.5;
+		weight2 = 10;
+		weight3 = 20.6;
+		item1 = new BarcodedItem(barcode1, weight1);
+		item2 = new BarcodedItem(barcode2, weight2);
+		item3 = new BarcodedItem(barcode3, weight3);
+		
+		// add all products to a list
+		allProduct.add(product1);
+		allProduct.add(product2);
+		allProduct.add(product3);
+		
+		// add all items to a list
+		allItem.add(item1);
+		allItem.add(item2);
+		allItem.add(item3);
+		
+//		ScanAndWeigh scan = new ScanAndWeigh(allProduct, allItem);
+	}
+	
+	@Before
+	public void setupBanknoteAndCoin() {
+		// setup banknote
+		currency =  Currency.getInstance("CAD");
+		banknoteDenomin = new int[] {5, 10, 20, 50};
+		
+		// invalid banknote
+		banknote_one = new Banknote(currency, 1);
+
+		// valid banknote
+		banknote_five = new Banknote(currency, 5);
+		banknote_ten = new Banknote(currency, 10);
+		banknote_twenty = new Banknote(currency, 20);
+		banknote_fifty = new Banknote(currency, 50);
+
+		// setup coins
+		coinDenomin = new BigDecimal[] {new BigDecimal("0.05"), new BigDecimal("0.10"), new BigDecimal("0.25"), new BigDecimal("1.00"), new BigDecimal("2.00")};
+		Coin.DEFAULT_CURRENCY = currency;
+
+		// invalid coin
+		penny = new Coin(new BigDecimal("0.01"));
+
+		// valid banknote
+		nickel = new Coin(new BigDecimal("0.05"));
+		dime = new Coin(new BigDecimal("0.10"));
+		quarter = new Coin(new BigDecimal("0.25"));
+		loonie = new Coin(new BigDecimal("1.00"));
+		tonnie = new Coin(new BigDecimal("2.00"));
+	}
+	
+	@Before
+	public void setup() {
+		// setup self-checkout station
+		currency =  Currency.getInstance("CAD");
+		banknoteDenomin = new int[] {5, 10, 20, 50};
+		coinDenomin = new BigDecimal[] {new BigDecimal("0.05"), new BigDecimal("0.10"), new BigDecimal("0.25"), new BigDecimal("1.00"), new BigDecimal("2.00")};
+		
+		int maxWeight = 500, sensitivity = 1;
+		scs = new SelfCheckoutStation(currency, banknoteDenomin, coinDenomin, maxWeight, sensitivity);
+		
+		// create observer objects
+		ScanAndWeigh scanAndScaleObs = new ScanAndWeigh(allProduct, allItem);
+		checkout = new CheckoutSystem();
+		
+		// attach observers
+		scs.scanner.attach(scanAndScaleObs);
+		scs.scale.attach(scanAndScaleObs);
+		scs.banknoteValidator.attach(checkout);
+		scs.coinValidator.attach(checkout);
 	}
 
-	@Override
-	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
+	@Test
+	public void testFunctionality() {	
+		try {
+			scs.scanner.scan(item1);
+			scs.scale.add(item1);
+			
+			checkout.checkout_btn(product1.getPrice().doubleValue());
+			
+			scs.banknoteValidator.accept(new Banknote(currency, 10));
+			scs.coinValidator.accept(dime);
+			
+			checkout.finish();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		
 	}
-
-	@Override
-	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
-		
+	
+	
+	@Test (expected = SimulationException.class)
+	public void testFinish() {
+		checkout.checkout_btn(100); //setting total to be much higher than total money inserted.
+		checkout.finish();
 	}
-
-	@Override
-	public void weightChanged(ElectronicScale scale, double weightInGrams) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void overload(ElectronicScale scale) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void outOfOverload(ElectronicScale scale) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
