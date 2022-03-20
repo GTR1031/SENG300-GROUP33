0f37e3e1d45d2472133f26fe2c787c2065076659
rei-tsunemi <reren1031@gmail.com>
2022-03-20 12:59:50 -0600

    Update CheckoutSystem.java
    commenting and minor changes

--- SCS-Software/src/ca/ucalgary/seng300/selfcheckoutP1/CheckoutSystem.java
+++ SCS-Software/src/ca/ucalgary/seng300/selfcheckoutP1/CheckoutSystem.java
@@ -1,3 +1,16 @@
+/*
+ * SENG300 Group33
+ * Project iteration 1
+ * 
+ * Group member:
+ * Joshua Cordeiro-Zebkowitz
+ * Ethan Macson
+ * Spencer Mutch
+ * Chenghou Si
+ * Rei Tsunemi
+ * Nhat Truong
+ */
+
 package ca.ucalgary.seng300.selfcheckoutP1;
 
 import java.math.BigDecimal;
@@ -19,20 +32,6 @@ public class CheckoutSystem implements CoinValidatorObserver, BanknoteValidatorO
 		totalAmount = 0;
 	}
 	
-	// input the total bill amount when hit the checkout button
-	public void checkout_btn(double total) {
-		totalAmount = total;
-	}
-	
-	// called when customers hit the finish payment button
-	public void finish() {
-		double totalInserted = coinTotal.doubleValue() + (double) banknoteTotal;
-	
-		if (totalInserted < totalAmount) {
-			throw new SimulationException("Not enough");
-		}
-	}
-
 	@Override
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
 	}
@@ -42,10 +41,9 @@ public class CheckoutSystem implements CoinValidatorObserver, BanknoteValidatorO
 		
 	}
 
-
 	@Override
 	public void validCoinDetected(CoinValidator validator, BigDecimal value) {
-		coinTotal.add(value);
+		coinTotal.add(value); //adding to coinTotal when valid
 	}
 
 	@Override
@@ -55,12 +53,27 @@ public class CheckoutSystem implements CoinValidatorObserver, BanknoteValidatorO
 
 	@Override
 	public void validBanknoteDetected(BanknoteValidator validator, Currency currency, int value) {
-		banknoteTotal += value;
+		banknoteTotal += value; //adding to banknoteTotal when valid
 	}
 
 	@Override
 	public void invalidBanknoteDetected(BanknoteValidator validator) {
 			
 	}
+	
+	// input the total bill amount when hit the checkout button
+	public void checkout_btn(double total) {
+		totalAmount = total;
+	}
+	
+	// called when customers hit the finish payment button
+	public void finish() {
+		double totalInserted = coinTotal.doubleValue() + (double) banknoteTotal; //converting BigDecimal number to double
+	
+		if (totalInserted < totalAmount) {
+			throw new SimulationException("Not enough");
+		}
+	}
+	
 
 }
