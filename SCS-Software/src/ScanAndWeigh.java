9e60c91473e5d4f698317f16798b252a8e5e887f
Joshua Cordeiro-Zebkowitz <zebkowitz@gmail.com>
2022-03-20 14:14:49 -0600

    Update ScanAndWeigh

--- SCS-Software/src/ScanAndWeigh.java
+++ SCS-Software/src/ScanAndWeigh.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+import java.util.HashMap;
 import org.lsmr.selfcheckout.Barcode;
 import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.BarcodeScanner;
@@ -5,24 +7,30 @@ import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.BarcodeScannerObserver;
 import org.lsmr.selfcheckout.devices.ElectronicScale;
 import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
+import org.lsmr.selfcheckout.products.BarcodedProduct;
+import org.lsmr.selfcheckout.BarcodedItem;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import java.math.BigDecimal;
 
 public class ScanAndWeigh implements BarcodeScannerObserver, ElectronicScaleObserver {
-
-	@Override
-	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
+	private BarcodeScanner scanner;
+	private ElectronicScale scale;
+	private Map<Barcode, BarcodedProduct> productDatabase = new HashMap<>();
+	private Map<Barcode, BarcodedItem> itemDatabase = new HashMap<>();
+	
+	public ScanAndWeigh() {
 		
 	}
 
 	@Override
-	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {}
+
+	@Override
+	public void disabled(AbstractDevice<? extends AbstractDeviceObserver> device) {}
 
 	@Override
 	public void weightChanged(ElectronicScale scale, double weightInGrams) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
@@ -40,7 +48,13 @@ public class ScanAndWeigh implements BarcodeScannerObserver, ElectronicScaleObse
 
 	@Override
 	public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode) {
-		// TODO Auto-generated method stub
+		BarcodedProduct product = productDatabase.get(barcode);
+		BarcodedItem item = itemDatabase.get(barcode);
+		
+		double weight = item.getWeight();
+		BigDecimal price = product.getPrice();
+		
+		
 		
 	}
 
