58dff99a366a6c54b9ec48ccbfce3333a48a769b
spenc <spenc@10.0.0.243>
2022-03-20 15:26:56 -0600

    Added a basic constructor for ScanAndWeigh

--- SCS-Software/src/ca/ucalgary/seng300/selfcheckoutP1/ScanAndWeigh.java
+++ SCS-Software/src/ca/ucalgary/seng300/selfcheckoutP1/ScanAndWeigh.java
@@ -8,6 +8,24 @@ import org.lsmr.selfcheckout.devices.ElectronicScale;
 import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
 
 public class ScanAndWeigh implements BarcodeScannerObserver, ElectronicScaleObserver {
+	private BarcodeScanner barcodeScanner;
+	private ElectronicScale electronicScale;
+	private double totalWeight;
+	
+	/* Constructor for ScanAndWeigh
+	 * 
+	 * @param scanner
+	 * 		The barcode scanner that we are using. Can't be null
+	 * @param scale
+	 * 		The scale that we are using. Can't be null
+	 * @throws NullPointerException
+	 * 		If either the scanner or scale is null
+	 */
+	public ScanAndWeigh (BarcodeScanner scanner, ElectronicScale scale) throws NullPointerException {
+		barcodeScanner = scanner;
+		electronicScale = scale;
+		
+	}
 
 	@Override
 	public void enabled(AbstractDevice<? extends AbstractDeviceObserver> device) {
